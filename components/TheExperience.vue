520bb371a12466b07ec9f51751eff4c47000e19a
Tetsuro Shingu <ttr4.baa@gmail.com>
2023-08-12 22:00:51 +0900

    球体の移動まで

--- components/TheExperience.vue
+++ components/TheExperience.vue
@@ -4,17 +4,25 @@
   </div>
 </template>
 <script setup lang="ts">
-import { Scene, PerspectiveCamera, Mesh, SphereGeometry, MeshBasicMaterial, WebGLRenderer } from 'three';
+import { Scene, PerspectiveCamera, Mesh, SphereGeometry, MeshBasicMaterial, WebGLRenderer, Color, Fog } from 'three';
 import * as THREE from 'three';
+import { useWindowSize } from '@vueuse/core';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 let renderer: WebGLRenderer;
 const experience: Ref<HTMLCanvasElement | null> = ref(null);
+// ウィンドウサイズ window.innerWidthと同じ
+const { width, height } = useWindowSize();
+const aspectRatio = computed(() => width.value / height.value);
+
+const bgColor = new Color('#E1F0C2');
 /**
  * Scene
  * シーンとは3D空間のこと 3Dオブジェクトや光源などの置き場となる
  */
 const scene = new Scene();
-
+scene.fog = new Fog(bgColor, 0.1, 75);
+scene.background = bgColor;
 /**
  * Camera
  * 3Dではどの視点から空間を撮影するか、という実装を行う この機能は「視点」や「カメラ」と呼ばれる
@@ -25,7 +33,9 @@ const scene = new Scene();
 // const height = 600;
 // アスペクト比は要素の幅を高さで割ったもの
 // 　const camera = new PerspectiveCamera(75, width / height, 0.1, 1000);
-　const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+　const camera = new PerspectiveCamera(75, aspectRatio.value, 0.1, 1000);
+// カメラの位置をズラす set(x, y, z)
+camera.position.set(0, 0, 4);
 // シーンにカメラを追加
 scene.add(camera);
 
@@ -41,27 +51,48 @@ const geometry = new SphereGeometry(1, 32, 32);
 const material = new MeshBasicMaterial({ color: 0x00ff00 });
 // メッシュとは、ジオメトリを受け取り、それにマテリアルを適用するオブジェクトのこと
 const sphere = new Mesh(geometry, material);
-// カメラの位置をズラす set(x, y, z)
-camera.position.set(0, 0, 2);
 // シーンに球体を追加
-scene.add(sphere);
+// scene.add(sphere);
 
-function setRender() {
+
+
+
+function updateCamera() {
+  camera.aspect = aspectRatio.value; // この二つが重要 アスペクト比が常に更新されるように
+  camera.updateProjectionMatrix(); // この二つが重要 アスペクト比が常に更新されるように
+}
+
+function updateRenderer() {
+  // アプリを描画するサイズを設定
+  renderer.setSize(width.value, height.value);
+  renderer.render(scene, camera);
+}
+
+function setRenderer() {
   if(experience.value) {
     /**
      * Renderer
      * rendererのインスタンスを作成
      * アプリを描画するサイズを設定する必要がある
      */
-    renderer = new WebGLRenderer({ canvas: experience.value });
-    // アプリを描画するサイズを設定
-    renderer. setSize(window.innerWidth, window.innerHeight);
-    renderer.render(scene, camera);
+    renderer = new WebGLRenderer({ canvas: experience.value, alpha: true });
+    updateRenderer();
   }
 };
 
+watch(aspectRatio, () => {
+  updateCamera(); // これ大事
+  updateRenderer();
+})
 
 onMounted(() => {
-  setRender();
+  setRenderer();
+  loop();
 })
+
+const loop = () => {
+  sphere.position.x += 0.01;
+  renderer.render(scene, camera);
+  requestAnimationFrame(loop);
+}
 </script>
